319e86a34ec740118889555c3969535101127571
Felipe Mendes <felipecarvalhomendes@gmail.com>
2024-02-06 10:16:52 -0300

    Add comments

--- app.js
+++ app.js
@@ -8,14 +8,15 @@ const arrFive = ['will become true', 'is coming'];
 function generateMessage() {
     const arrOneRandomizer = Math.floor(Math.random() * arrOne.length);
 
+    // Check if phrase starts with 'You'
     if (arrOneRandomizer === 0) {
         const arrThreeRandomizer = Math.floor(Math.random() * arrThree.length);
 
         console.log(`${arrOne[arrOneRandomizer]} ${arrTwo} ${arrThree[arrThreeRandomizer]}`);
-
+    
+    // Check if phrase starts with 'Your'
     } else {
         const arrFourRandomizer = Math.floor(Math.random() * arrFour.length);
-
         const arrFiveRandomizer = Math.floor(Math.random() * arrFive.length);
 
         console.log(`${arrOne[arrOneRandomizer]} ${arrFour[arrFourRandomizer]} ${arrFive[arrFiveRandomizer]}`);
